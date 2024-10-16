3253cc9ae69084de328bc840aa00c649c49f164c
Nin <flyingchives@gmail.com>
2024-10-15 20:03:37 -0400

    Added rocket2 program

---
+++ Rocket2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Rocket2 {
+public 
+static void main(String...args) {
+
+
+    for (int countdown = 10; countdown > 0; countdown--) {
+        System.out.println(countdown);
+    }
+    System.out.println("Blast off!");
+}}
