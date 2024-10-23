9702bd9d96af57e1ac52e8b0bf6290d0a2f913ba
Nin <flyingchives@gmail.com>
2024-10-22 20:03:52 -0400

    Added Functions

---
+++ Functions.java
@@ -0,0 +1,24 @@
+public class Functions {
+
+    public static double add(double x, double y) {
+        return 2 * x + 4 * y;
+    }
+
+    public static double subtract(double x, double y) {
+        return x - y;
+    }
+
+    public static void main(String args) {
+
+        double x = add(5, 7);
+        System.out.println(x);
+    }
+
+    public static void main(string args) {
+        double x = add(5, 7);
+        double y = subtract(5, 7);
+        double z = add(2, 3) + subtract(4, 1);
+        System.out.println(x);
+    }
+
+}
