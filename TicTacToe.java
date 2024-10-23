ad6591aff09412f078b233be283b98571e108c5b
Nin <flyingchives@gmail.com>
2024-10-22 20:03:33 -0400

    Added TicTacToe

---
+++ TicTacToe.java
@@ -0,0 +1,14 @@
+public class TicTacToe {
+    // Function that takes a scanner and either X or O
+    // ask for two numbers - row and column
+
+    // Function that checks a board (char[][]) to see if it hass three in row
+
+    public static void main(string args) {
+        char[][] board = new char[][] {
+                new char[3],
+                new char[3],
+                new char[3]
+        };
+    }
+}
