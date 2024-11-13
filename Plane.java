a7e8098831e8fbb98e2a0f5bf8ee0cf26c12046f
Nin <flyingchives@gmail.com>
2024-11-12 19:32:12 -0500

    Changed Plane

--- Plane.java
+++ Plane.java
@@ -1,10 +1,36 @@
 public class Plane extends Vehicle {
-    public Plane(String type, String powerType, int numWheels, int numDoors, String color, double fuelEconomy, int passangerCapacity) {
-        super(type, powerType, numWheels, numDoors, color, fuelEconomy, passangerCapacity)
-    }
-    // If landing gear is down
-    public
-    // Make a takeoff function
-    // Make a landing function (only land if gear is down)
 
+    public Plane(String powerType, int numWheels, int numDoors, String color, double fuelEconomy, int passangerCapacity) {
+        super("Plane", powerType, numWheels, numDoors, color, fuelEconomy, passangerCapacity)
+        boolean islandinggeardown;
+        isLandingGearDown = true;
+    }
+
+    public void takeoff() {
+        System.out.println("We have liftoff!");
+        isLandingGearDown = false;
+    }
+
+    public void prepToLand() {
+        isLandingGearDown = true;
+
+    }
+
+    public boolean land() {
+        if (isLandingGearDown) {
+            System.out.println("We have landed!");
+            return true;
+        } else {
+            System.out.println("Put the landing gear down.");
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Plane cessna = new Plane("Pixie Dust", 6, 2, "White", 70, 4);
+        cessna.takeoff();
+        cessna.land();
+        cessna.prepToLand();
+        cessna.land();
+    }
 }
